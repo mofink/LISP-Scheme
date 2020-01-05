2b3f2cd7a3647f9cca5f28aa4b1d6ec5894fdb0c
mofink <mosheafink@gmail.com>
2020-01-05 00:43:31 -0500

    Create Ex-1.16.scm

---
+++ Ex-1.16.scm
@@ -0,0 +1,26 @@
+; Design a procedure that evolves an iterative exponentiation process in logarithmic time
+
+;Solution is of the form b^n, a is an invariant that holds ab^n constant
+
+(define (expt-iter b n) 
+    (expt-iter-aux b n 1)
+)
+
+(define (expt-iter-aux b n a)
+  (cond 
+    ((= n 1) a)
+    ((odd n) (expt-iter-aux b (- n 1) (* a b)))
+    (else (expt-iter-aux b (/ n 2) (* a (square b))))
+  )
+)
+
+(define (square n) 
+    (* n n)
+)
+
+(define (odd n) 
+    (= (remainder n 2) 1)
+)
+
+(expt-iter 3 5)
+(expt-iter 2 4)
